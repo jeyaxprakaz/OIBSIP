5d2b2fa9cc39335d827aa690772c89198b886128
Jeya Prakash S <109335203+jeyaxprakaz@users.noreply.github.com>
2023-12-27 18:04:55 +0530

    Add files via upload

---
+++ NumberGuessingGame.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class NumberGuessingGame {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+
+        int lowerBound = 1;
+        int upperBound = 100;
+        int secretNumber = random.nextInt(upperBound - lowerBound + 1) + lowerBound;
+        int guess;
+        int attempts = 0;
+        boolean hasGuessedCorrectly = false;
+
+        System.out.println("Welcome to the Number Guessing Game!");
+        System.out.println("I have selected a number between " + lowerBound + " and " + upperBound + ". Try to guess it.");
+
+        while (!hasGuessedCorrectly) {
+            System.out.print("Enter your guess: ");
+            guess = scanner.nextInt();
+            attempts++;
+
+            if (guess < lowerBound || guess > upperBound) {
+                System.out.println("Please guess a number between " + lowerBound + " and " + upperBound + ".");
+            } else if (guess < secretNumber) {
+                System.out.println("Too low! Try again.");
+            } else if (guess > secretNumber) {
+                System.out.println("Too high! Try again.");
+            } else {
+                hasGuessedCorrectly = true;
+                System.out.println("Congratulations! You've guessed the number in " + attempts + " attempts.");
+            }
+        }
+
+        scanner.close();
+    }
+}
